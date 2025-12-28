8a2054c4961bc551f494ec61d74dcad17b8420f0
phewphewb <valentyn.patsera@gmail.com>
2025-12-28 18:51:22 -0500

    add src file when flag is enabled

--- keyboards/zsa/moonlander/post_rules.mk
+++ keyboards/zsa/moonlander/post_rules.mk
@@ -1,1 +1,7 @@
 include keyboards/zsa/common/features.mk
+
+ACTIVE_LAYER_INDICATOR_ENABLE ?= no
+ifeq ($(strip $(ACTIVE_LAYER_INDICATOR_ENABLE)), yes)
+    OPT_DEFS += -DACTIVE_LAYER_INDICATOR_ENABLE
+    SRC += layout_indicator/layout_indicator.c
+endif
